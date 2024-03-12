05ee09c4fb0e524b8b8d9e76c0bbb375ae5512de
Usuario <Usuario@CPD03-M04>
2024-03-12 11:43:52 -0300

    Adicionar arquivos de projeto.

---
+++ AulaDesSis05Aluno/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaDesSis05Aluno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a Porcentagem da Frequência do aluno: ");
+            double freq = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a média do aluno: ");
+            double med = double.Parse(Console.ReadLine());
+
+            if (freq >= 75 && med >= 7)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else if ((freq >= 75 && med < 7 | med >= 4) | (freq < 75 && med >= 7))
+            {
+                Console.WriteLine("Recuperação");
+            }
+            else 
+            {
+                Console.WriteLine("Repetido");
+            }
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
